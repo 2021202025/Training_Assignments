719e87319cceb2e1260367d95e204d8b82d6e7b7
Manu Gupta <gmanu21097@gmail.com>
2019-11-15 11:36:16 +0530

    Assignment_

---
+++ Assignment_3.py
@@ -0,0 +1,9 @@
+def pattern(no_of_lines):
+    line_count = 3
+    for i in range(1,no_of_lines+1):
+        print("_"*i,line_count)
+        line_count += 3
+
+
+n = int(input("Enter the number of lines: "))
+pattern(n)
